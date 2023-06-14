bbcd6a2afd7da147e3079f82203dc50171fb0060
Gh0st4v0 <89959988+Gh0st4v0@users.noreply.github.com>
2023-06-14 18:57:54 -0300

    Update script.js

--- ProjetoDW/script.js
+++ ProjetoDW/script.js
@@ -1,31 +1,49 @@
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("ProFiis:rows")) || []
+    },
 
+    set(rows) {
+        localStorage.setItem("ProFiis:rows", JSON.stringify(rows))
+    }
+}
+
+const Modal = {
+    activateModal(){
+        document
+            .querySelector('.modal')
+            .classList
+            .remove('inactive')
+    },
+
+    closeModal(){
+        document
+            .querySelector('.modal')
+            .classList
+            .add('inactive')
+    }
+}
 
 const DOM = {
     rowsContainer: document.querySelector('tbody'),
 
-    addRow(index) {
+    finishRow(row, index){
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLRow()
+        tr.innerHTML = DOM.innerHTMLFinishedRow(row,index)
         tr.dataset.index = index
 
         DOM.rowsContainer.appendChild(tr)
-
     },
 
-    innerHTMLRow() {
+    innerHTMLFinishedRow(row,index) {
         const html = `
         <tr>
-            <td><input class="nomeFundo"        type="text"     placeholder="NOME"></td>
-            <td><input class="numCotas"         type="number"   placeholder="50"></td>
-            <td><input class="cotacao"          type="text"     placeholder="10,99"></td>
-            <td><input class="ultimoRendimento" type="text"     placeholder="0,12"></td>
-            <div id="salvarDeletar">
-                <td id="opcoesSalvarDeletar">
-                    <img src="./Images/saveIcon.png" alt="Salvar Alterações">
-                    <img onclick="DOM.deleteRow(this)" src="./Images/deleteIcon.png" alt="Deletar fundo">
-                </td>
-            </div>
-        </tr>
+                            <td>${row.nomeFundo.toUpperCase()}</td>
+                            <td>${row.numCotas}</td>
+                            <td>${Utils.formatCurrency(row.cotacao)}</td>
+                            <td>${Utils.formatCurrency(row.ultimoRendimento)}</td>
+                            <td><img src="./Images/deleteIcon.png" alt="Editar Fundo" onclick="Row.remove(${index})"></td>
+                        </tr>
         `
         return html
     },
@@ -33,30 +51,78 @@ const DOM = {
     deleteAll() {
         let text = "Você tem certeza que deseja excluir todos os fundos?";
         if (confirm(text) == true) {
-            DOM.rowsContainer.innerHTML = ""
+            Row.all = []
+        }
+        App.reload()
+    },
+
+    deleteRow() {
+        document.querySelector("form").innerHTML = ""
+        App.reload()
+    },
+
+
+    updateBalance() {
+        document
+            .getElementById('patrimonioTotal')
+            .innerHTML = Utils.formatCurrency(Row.total())
+        document
+            .getElementById('rendimentoDoMes')
+            .innerHTML = Utils.formatCurrency(Row.rendimentoTotal())
+        if (Row.total() > 0){
+        value = Row.rendimentoTotal()/Row.total()
+        document
+            .getElementById('dividendYield')
+            .innerHTML = Utils.formatPercentage(value)
+        }
+        else{
+            document
+            .getElementById('dividendYield')
+            .innerHTML = "0,00%"
         }
     },
 
-    deleteRow(btn) {
-        var row = btn.parentNode.parentNode;
-        row.parentNode.removeChild(row);
+    clearRows() {
+        DOM.rowsContainer.innerHTML = ""
+    }
+
+}
+
+const Row = {
+    all: Storage.get(),
+
+    add(row){
+        Row.all.push(row)
+
+        App.reload()
+    },
+
+    remove(index) {
+        Row.all.splice(index, 1)
+
+        App.reload()
+    },
+
+    total() {
+        let total = 0;
+        Row.all.forEach(row => {
+            total += row.numCotas * row.cotacao;
+        })
+        return total;
+    },
+
+    rendimentoTotal(){
+        let total = 0;
+        Row.all.forEach(row => {
+            total += row.numCotas * row.ultimoRendimento;
+        })
+        return total;
     }
 }
 
 const PageStyles = {
     darkMode() {
-        document.getElementById("headOfPage").innerHTML =
-            `
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ProFiis</title>
-        <link id="pagestyle" rel="stylesheet" href="./Styles/darkStyle.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,500;1,600&display=swap"
-        rel="stylesheet">
-        `
+        document.getElementById("pagestyle").href = "./Styles/darkStyle.css"
 
         document.getElementById("upperOptions").innerHTML = 
         `
@@ -67,18 +133,7 @@ const PageStyles = {
     },
 
     lightMode() {
-        document.getElementById("headOfPage").innerHTML =
-            `
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ProFiis</title>
-        <link id="pagestyle" rel="stylesheet" href="./Styles/style.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,500;1,600&display=swap"
-        rel="stylesheet">
-        `
+        document.getElementById("pagestyle").href = "./Styles/style.css"
 
         document.getElementById("upperOptions").innerHTML = 
         `
@@ -89,3 +144,105 @@ const PageStyles = {
     }
 }
 
+const Utils = {
+    formatAmount(value){
+        value = Number(value.replace(/\,\./g, "")) * 100
+        
+        return value
+    },
+
+    formatCurrency(value) {
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+       return value
+    },
+
+    formatPercentage(value) {
+        num = `${Number(value*100).toFixed(2)}%`;
+
+        return num
+    }
+}
+
+const Form = {
+    nomeFundo: document.querySelector('#nomeFundo'),
+    numCotas: document.querySelector('#numCotas'),
+    cotacao: document.querySelector('#cotacao'),
+    ultimoRendimento: document.querySelector('#ultimoRendimento'),
+
+    getValues(){
+        return{
+            nomeFundo: Form.nomeFundo.value,
+            numCotas: Form.numCotas.value,
+            cotacao: Form.cotacao.value,
+            ultimoRendimento: Form.ultimoRendimento.value
+        }
+    },
+
+    validateFields() {
+        const { nomeFundo, numCotas, cotacao, ultimoRendimento } = Form.getValues()
+        if( nomeFundo.trim() === "" || 
+            numCotas.trim() === "" || 
+            cotacao.trim() === "" ||
+            ultimoRendimento.trim() === "") {
+                throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+
+    formatValues() {
+        let { nomeFundo, numCotas, cotacao, ultimoRendimento } = Form.getValues()
+        
+        cotacao = Utils.formatAmount(cotacao)
+        ultimoRendimento = Utils.formatAmount(ultimoRendimento)
+
+
+        return {
+            nomeFundo, 
+            numCotas, 
+            cotacao, 
+            ultimoRendimento
+        }
+    },
+
+    clearFields() {
+        Form.nomeFundo.value = ""
+        Form.numCotas.value = ""
+        Form.cotacao.value = ""
+        Form.ultimoRendimento.value = ""
+    },
+
+    submit(event) {
+        event.preventDefault()
+
+        try {
+            Form.validateFields()
+            const row = Form.formatValues()
+            Row.add(row)
+            Form.clearFields()
+            Modal.closeModal()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+}
+
+const App = {
+    init() {
+        Row.all.forEach(DOM.finishRow)
+        
+        DOM.updateBalance()
+
+        Storage.set(Row.all)
+    },
+    reload() {
+        DOM.clearRows()
+        App.init()
+    },
+}
+
+App.init()
